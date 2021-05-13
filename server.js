6d7b99e806e2b568c720e09b63440bc9fefe983d
Oladipo <dipo@afridext.com>
2021-05-13 15:07:20 -0500

    Intro to Express part 1

---
+++ server.js
@@ -0,0 +1,22 @@
+const express= require('express');
+
+const hostname= 'localhost';
+
+const port= 3000
+
+const app=express();
+
+app.use((req,res)=>{
+    console.log(req.header);
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an express serve</h1></body></html>');
+});
+
+
+app.listen(port, hostname, ()=>{
+    console.log(`server running at http://${hostname}: ${port}/`);
+
+})
+
+
