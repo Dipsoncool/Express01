cf3d43d1d95d5c4953cc2e27514e46a6513eb248
Oladipo <dipo@afridext.com>
2021-05-14 22:02:10 -0500

    Express Server Testing 1

--- server.js
+++ server.js
@@ -9,6 +9,54 @@ const app=express();
 
 app.use(morgan('dev'));
 
+app.use(express.json());
+
+app.all('/campsites', (req, res, next)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'html/plain');
+    next();
+});
+
+app.get('/campsites', (req, res)=>{
+    res.end('Will send all campsites to you');
+});
+
+app.post('/campsites', (req, res)=>{
+    res.end(`Will add the campsite : ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res)=>{
+    res.statusCode=403;
+    res.end('PUT operation is not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res)=>{
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res)=>{
+    res.end(`Will send details of the ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsite/:campsiteId', (req, res)=>{
+    res.statusCode=403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+
+
+
+
 app.use(express.static(__dirname + '/public'));
 
 app.use((req,res)=>{
